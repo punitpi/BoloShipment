b19c29a4bb347abb7c085ec5df5862e6c85eb0c0
Puneet Prakash <punit.pi@gmail.com>
2020-01-30 18:16:40 +0530

    chenges to view.py

--- shipment/views.py
+++ shipment/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-import shipment.tokenRefresh as tr
 from django.http import HttpResponse
+import shipment.tokenRefresh as tr
+import shipment.getShipment as gs
 
 
 # Create your views here.
 def index(request):
     tr.getAccessToken()
-    
+    gs.getShipments()
     return HttpResponse("Got Token")
     #return render(request, 'shipment/index.html')
